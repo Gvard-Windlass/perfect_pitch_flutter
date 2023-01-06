92fe34b75140f26b69b20db1588f23206b4bdd57
Gvard Windlass <gvard.windlass@gmail.com>
2023-01-06 14:58:48 +0300

    Add settings data

--- lib/settings_wrapper.dart
+++ lib/settings_wrapper.dart
@@ -20,7 +20,37 @@ class SettingsWrapper extends StatefulWidget {
 }
 
 // data and methods go here
+enum ExerciseMode {singlePitch, unison}
+
 class SettingsWrapperState extends State<SettingsWrapper> {
+  static final List<String> _pitches = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+  static final List<String> _octaves = ['1', '2', '3', '4', '5', '6', '7'];
+  
+  late Map<String, bool> pitchSelection;
+  late Map<String, bool> octaveSelection;
+  late ExerciseMode exerciseMode;
+
+  Map<String, bool> get whiteKeys => {
+    for (final key in pitchSelection.keys)
+      if (!key.contains('#')) key: pitchSelection[key]!
+  };
+
+  Map<String, bool> get blackKeys => {
+    for (final key in pitchSelection.keys)
+      if (key.contains('#')) key: pitchSelection[key]!
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    final defaultPitches = ['E', 'F#'];
+    final defaultOctaves = ['1', '2', '4', '5'];
+
+    pitchSelection = Map.fromIterable(_pitches, value: (pitch) => defaultPitches.contains(pitch));
+    octaveSelection = Map.fromIterable(_octaves, value: (octave) => defaultOctaves.contains(octave));
+    exerciseMode = ExerciseMode.singlePitch;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return SettingsProvider(data: this, child: widget.child);
